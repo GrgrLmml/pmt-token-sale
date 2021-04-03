6c9fc353732cbe15f5de95e0b717504931fe97fd
GrgrLmml <gregor.laemmel@gmail.com>
2021-04-03 21:34:16 +0200

    initial

---
+++ precache-manifest.6dc8d7f7016bddf6528aeed9b377cc93.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "cfed4b42e2b7094a2e10766063b0b06c",
+    "url": "/index.html"
+  },
+  {
+    "revision": "3c082165c9e77a1bb3d2",
+    "url": "/static/css/main.b100e6da.chunk.css"
+  },
+  {
+    "revision": "45411e4e22bdccead8fc",
+    "url": "/static/js/2.0cfc05c5.chunk.js"
+  },
+  {
+    "revision": "3c082165c9e77a1bb3d2",
+    "url": "/static/js/main.ebd080a0.chunk.js"
+  },
+  {
+    "revision": "0a31ddf7382e0363a369",
+    "url": "/static/js/runtime-main.b240f91f.js"
+  }
+]);
